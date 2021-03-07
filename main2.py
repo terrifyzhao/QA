174b0005272a68911bca0bf1f3da788f9b3558a0
joe.zhao <zjiuzhou@gmail.com>
2021-03-07 12:04:53 +0800

    add elmo

--- main2.py
+++ main2.py
@@ -2,13 +2,17 @@ import numpy as np
 import pandas as pd
 import gensim
 import jieba
+import os
+from elmoformanylangs import Embedder
 
 df = pd.read_csv('data/qa_data.csv')
 question = df['question'].values
 answer = df['answer'].values
 
-model_path = 'word2vec/wiki.model'
-model = gensim.models.Word2Vec.load(model_path)
+# model_path = 'word2vec/wiki.model'
+# model = gensim.models.Word2Vec.load(model_path)
+
+model = Embedder(os.path.join(os.getcwd(), 'elmo'))
 
 
 def sen2vec(sentence):
@@ -26,22 +30,35 @@ def sen2vec(sentence):
     return vec
 
 
+def elmo2vec(sentence):
+    if isinstance(sentence, str):
+        segment = list(jieba.cut(sentence))
+        vec = model.sents2elmo([segment])
+    elif isinstance(sentence, np.ndarray):
+        segment = [jieba.cut(s) for s in sentence]
+        vec = model.sents2elmo(segment)
+    return [np.mean(v, axis=0) for v in vec]
+
+
 def cosine(a, b):
     return np.matmul(a, np.array(b).T) / (np.linalg.norm(a) * np.linalg.norm(b, axis=1))
 
 
 if __name__ == '__main__':
-    question_vec = []
-    for q in question:
-        question_vec.append(sen2vec(q))
+
+    # question_vec = []
+    # for q in question:
+    #     question_vec.append(sen2vec(q))
+
+    question_vec = elmo2vec(question)
 
     while 1:
         text = input('请输入您的问题：')
 
-        vec = sen2vec(text)
+        vec = elmo2vec(text)
 
         # 计算相似度
-        similarity = cosine(vec, question_vec)
+        similarity = cosine(vec, question_vec)[0]
         # print('最大的相似度:', max(similarity))
         max_similarity = max(similarity)
         # 最大的相似度对应的下标
